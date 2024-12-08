47a81d48192ddf350842e634ec40e3e381d6b0f9
JoshBard <jbardwic@bu.edu>
2024-12-08 17:44:35 -0500

    indent error

--- homework/utils.py
+++ homework/utils.py
@@ -72,110 +72,110 @@ class PyTux:
         p = proj @ view @ np.array(list(x) + [1])
         return np.clip(np.array([p[0] / p[-1], -p[1] / p[-1]]), -1, 1)
 
-        def rollout(self, track, controller, planner=None, max_frames=1000, verbose=False, data_callback=None):
-            """
-            Play a level (track) for a single round.
-            """
-            if self.k is not None and self.k.config.track == track:
-                self.k.restart()
-                self.k.step()
-            else:
-                if self.k is not None:
-                    self.k.stop()
-                    del self.k
-                config = pystk.RaceConfig(num_kart=1, laps=1, track=track)
-                config.players[0].controller = pystk.PlayerConfig.Controller.PLAYER_CONTROL
+    def rollout(self, track, controller, planner=None, max_frames=1000, verbose=False, data_callback=None):
+        """
+        Play a level (track) for a single round.
+        """
+        if self.k is not None and self.k.config.track == track:
+            self.k.restart()
+            self.k.step()
+        else:
+            if self.k is not None:
+                self.k.stop()
+                del self.k
+            config = pystk.RaceConfig(num_kart=1, laps=1, track=track)
+            config.players[0].controller = pystk.PlayerConfig.Controller.PLAYER_CONTROL
 
-                self.k = pystk.Race(config)
-                self.k.start()
-                self.k.step()
+            self.k = pystk.Race(config)
+            self.k.start()
+            self.k.step()
 
-            state = pystk.WorldState()
-            track = pystk.Track()
+        state = pystk.WorldState()
+        track = pystk.Track()
 
-            last_rescue = 0
+        last_rescue = 0
+
+        if verbose:
+            fig, ax = plt.subplots(1, 1)
+
+        alpha = 0.5
+        gamma = 0.9
+        prev_aim_point_image = [0.0, 0.0]
+        previous_vel = 0.0
+
+        for t in range(max_frames):
+            state.update()
+            track.update()
+
+            kart = state.players[0].kart
+
+            if np.isclose(kart.overall_distance / track.length, 1.0, atol=2e-3):
+                if verbose:
+                    print("Finished at t=%d" % t)
+                break
+
+            proj = np.array(state.players[0].camera.projection).T
+            view = np.array(state.players[0].camera.view).T
+
+            # Store image prev
+            aim_point_world = self._point_on_track(kart.distance_down_track + TRACK_OFFSET, track)
+            aim_point_image = self._to_image(aim_point_world, proj, view)
+            if data_callback is not None:
+                data_callback(t, np.array(self.k.render_data[0].image), aim_point_image)
+
+            if planner:
+                image = np.array(self.k.render_data[0].image)
+                aim_point_image = planner(TF.to_tensor(image)[None]).squeeze(0).cpu().detach().numpy()
+
+            current_vel = np.linalg.norm(kart.velocity)
+            
+            # Use the model to generate action
+            action = self.model(torch.tensor([aim_point_image[0], aim_point_image[1], current_vel, 25]).unsqueeze(0))
+
+            pystk_action = pystk.Action()
+            pystk_action.brakes = action[0, 0].item()
+            pystk_action.drift = action[0, 1].item()
+            pystk_action.fire = action[0, 2].item()
+            pystk_action.nitro = action[0, 3].item()
+            pystk_action.rescue = action[0, 4].item()
+            pystk_action.acceleration = action[0, 5].item()
+            pystk_action.steer = action[0, 6].item()
+
+            if current_vel < 1.0 and t - last_rescue > RESCUE_TIMEOUT:
+                last_rescue = t
+                pystk_action.rescue = True
 
             if verbose:
-                fig, ax = plt.subplots(1, 1)
-
-            alpha = 0.5
-            gamma = 0.9
-            prev_aim_point_image = [0.0, 0.0]
-            previous_vel = 0.0
-
-            for t in range(max_frames):
-                state.update()
-                track.update()
-
-                kart = state.players[0].kart
-
-                if np.isclose(kart.overall_distance / track.length, 1.0, atol=2e-3):
-                    if verbose:
-                        print("Finished at t=%d" % t)
-                    break
-
-                proj = np.array(state.players[0].camera.projection).T
-                view = np.array(state.players[0].camera.view).T
-
-                # Store image prev
-                aim_point_world = self._point_on_track(kart.distance_down_track + TRACK_OFFSET, track)
-                aim_point_image = self._to_image(aim_point_world, proj, view)
-                if data_callback is not None:
-                    data_callback(t, np.array(self.k.render_data[0].image), aim_point_image)
-
+                ax.clear()
+                ax.imshow(self.k.render_data[0].image)
+                WH2 = np.array([self.config.screen_width, self.config.screen_height]) / 2
+                ax.add_artist(plt.Circle(WH2 * (1 + self._to_image(kart.location, proj, view)), 2, ec='b', fill=False, lw=1.5))
+                ax.add_artist(plt.Circle(WH2 * (1 + self._to_image(aim_point_world, proj, view)), 2, ec='r', fill=False, lw=1.5))
                 if planner:
-                    image = np.array(self.k.render_data[0].image)
-                    aim_point_image = planner(TF.to_tensor(image)[None]).squeeze(0).cpu().detach().numpy()
+                    ap = self._point_on_track(kart.distance_down_track + TRACK_OFFSET, track)
+                    ax.add_artist(plt.Circle(WH2 * (1 + aim_point_image), 2, ec='g', fill=False, lw=1.5))
+                plt.pause(1e-3)
 
-                current_vel = np.linalg.norm(kart.velocity)
-                
-                # Use the model to generate action
-                action = self.model(torch.tensor([aim_point_image[0], aim_point_image[1], current_vel, 25]).unsqueeze(0))
+            self.k.step(pystk_action)
 
-                pystk_action = pystk.Action()
-                pystk_action.brakes = action[0, 0].item()
-                pystk_action.drift = action[0, 1].item()
-                pystk_action.fire = action[0, 2].item()
-                pystk_action.nitro = action[0, 3].item()
-                pystk_action.rescue = action[0, 4].item()
-                pystk_action.acceleration = action[0, 5].item()
-                pystk_action.steer = action[0, 6].item()
+            # UPDATE HERE: Policy Gradient / DPG Update
+            progress = (kart.overall_distance / track.length) * (t / max_frames)
+            y_target = progress + gamma * self.model(torch.tensor([aim_point_image[0], aim_point_image[1], current_vel, 25]).unsqueeze(0))
 
-                if current_vel < 1.0 and t - last_rescue > RESCUE_TIMEOUT:
-                    last_rescue = t
-                    pystk_action.rescue = True
+            # Calculate gradient
+            gradient = y_target - action
 
-                if verbose:
-                    ax.clear()
-                    ax.imshow(self.k.render_data[0].image)
-                    WH2 = np.array([self.config.screen_width, self.config.screen_height]) / 2
-                    ax.add_artist(plt.Circle(WH2 * (1 + self._to_image(kart.location, proj, view)), 2, ec='b', fill=False, lw=1.5))
-                    ax.add_artist(plt.Circle(WH2 * (1 + self._to_image(aim_point_world, proj, view)), 2, ec='r', fill=False, lw=1.5))
-                    if planner:
-                        ap = self._point_on_track(kart.distance_down_track + TRACK_OFFSET, track)
-                        ax.add_artist(plt.Circle(WH2 * (1 + aim_point_image), 2, ec='g', fill=False, lw=1.5))
-                    plt.pause(1e-3)
+            # Perform gradient update on model weights (specifically the first layer)
+            self.optimizer.zero_grad()  # Clear previous gradients
+            loss = (0.5 * (gradient ** 2)).mean()  # Mean squared error loss
+            loss.backward()  # Backpropagate the error
+            self.optimizer.step()  # Update the model parameters
 
-                self.k.step(pystk_action)
+            prev_aim_point_image = aim_point_image
+            previous_vel = current_vel
 
-                # UPDATE HERE: Policy Gradient / DPG Update
-                progress = (kart.overall_distance / track.length) * (t / max_frames)
-                y_target = progress + gamma * self.model(torch.tensor([aim_point_image[0], aim_point_image[1], current_vel, 25]).unsqueeze(0))
-
-                # Calculate gradient
-                gradient = y_target - action
-
-                # Perform gradient update on model weights (specifically the first layer)
-                self.optimizer.zero_grad()  # Clear previous gradients
-                loss = (0.5 * (gradient ** 2)).mean()  # Mean squared error loss
-                loss.backward()  # Backpropagate the error
-                self.optimizer.step()  # Update the model parameters
-
-                prev_aim_point_image = aim_point_image
-                previous_vel = current_vel
-
-                t += 1
-            return t, kart.overall_distance / track.length
+            t += 1
+        return t, kart.overall_distance / track.length
 
     def close(self):
         """
