4a2a1081538c7d5387cc48f98f5236fad0fb4f85
Josh Bardwick <40773919+JoshBard@users.noreply.github.com>
2024-12-08 14:07:02 -0500

    Calling and updating controller.py NN

--- workspace/homework/utils.py
+++ workspace/homework/utils.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pystk
 
+import torch
+import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms.functional as TF
 import dense_transforms
+
+from controller import ControlModel, control
 
 RESCUE_TIMEOUT = 30
 TRACK_OFFSET = 15
@@ -47,6 +51,8 @@ class PyTux:
         self.config.screen_height = screen_height
         pystk.init(self.config)
         self.k = None
+        self.model = ControlModel()  # This is now the same as the controller
+        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
 
     @staticmethod
     def _point_on_track(distance, track, offset=0.0):
@@ -66,17 +72,9 @@ class PyTux:
         p = proj @ view @ np.array(list(x) + [1])
         return np.clip(np.array([p[0] / p[-1], -p[1] / p[-1]]), -1, 1)
 
-    def rollout(self, track, controller, planner=None, max_frames=1000, verbose=False, data_callback=None):
+       def rollout(self, track, controller, planner=None, max_frames=1000, verbose=False, data_callback=None):
         """
         Play a level (track) for a single round.
-        :param track: Name of the track
-        :param controller: low-level controller, see controller.py
-        :param planner: high-level planner, see planner.py
-        :param max_frames: Maximum number of frames to play for
-        :param verbose: Should we use matplotlib to show the agent drive?
-        :param data_callback: Rollout calls data_callback(time_step, image, 2d_aim_point) every step, used to store the
-                              data
-        :return: Number of steps played
         """
         if self.k is not None and self.k.config.track == track:
             self.k.restart()
@@ -85,7 +83,7 @@ class PyTux:
             if self.k is not None:
                 self.k.stop()
                 del self.k
-            config = pystk.RaceConfig(num_kart=1, laps=1,  track=track)
+            config = pystk.RaceConfig(num_kart=1, laps=1, track=track)
             config.players[0].controller = pystk.PlayerConfig.Controller.PLAYER_CONTROL
 
             self.k = pystk.Race(config)
@@ -98,11 +96,14 @@ class PyTux:
         last_rescue = 0
 
         if verbose:
-            import matplotlib.pyplot as plt
             fig, ax = plt.subplots(1, 1)
 
-        for t in range(max_frames):
+        alpha = 0.5
+        gamma = 0.9
+        prev_aim_point_image = [0.0, 0.0]
+        previous_vel = 0.0
 
+        for t in range(max_frames):
             state.update()
             track.update()
 
@@ -116,7 +117,8 @@ class PyTux:
             proj = np.array(state.players[0].camera.projection).T
             view = np.array(state.players[0].camera.view).T
 
-            aim_point_world = self._point_on_track(kart.distance_down_track+TRACK_OFFSET, track)
+            # Store image prev
+            aim_point_world = self._point_on_track(kart.distance_down_track + TRACK_OFFSET, track)
             aim_point_image = self._to_image(aim_point_world, proj, view)
             if data_callback is not None:
                 data_callback(t, np.array(self.k.render_data[0].image), aim_point_image)
@@ -126,24 +128,52 @@ class PyTux:
                 aim_point_image = planner(TF.to_tensor(image)[None]).squeeze(0).cpu().detach().numpy()
 
             current_vel = np.linalg.norm(kart.velocity)
-            action = controller(aim_point_image, current_vel)
+            
+            # Use the model to generate action
+            action = self.model(torch.tensor([aim_point_image[0], aim_point_image[1], current_vel, 25]).unsqueeze(0))
+
+            pystk_action = pystk.Action()
+            pystk_action.brakes = action[0, 0].item()
+            pystk_action.drift = action[0, 1].item()
+            pystk_action.fire = action[0, 2].item()
+            pystk_action.nitro = action[0, 3].item()
+            pystk_action.rescue = action[0, 4].item()
+            pystk_action.acceleration = action[0, 5].item()
+            pystk_action.steer = action[0, 6].item()
 
             if current_vel < 1.0 and t - last_rescue > RESCUE_TIMEOUT:
                 last_rescue = t
-                action.rescue = True
+                pystk_action.rescue = True
 
             if verbose:
                 ax.clear()
                 ax.imshow(self.k.render_data[0].image)
                 WH2 = np.array([self.config.screen_width, self.config.screen_height]) / 2
-                ax.add_artist(plt.Circle(WH2*(1+self._to_image(kart.location, proj, view)), 2, ec='b', fill=False, lw=1.5))
-                ax.add_artist(plt.Circle(WH2*(1+self._to_image(aim_point_world, proj, view)), 2, ec='r', fill=False, lw=1.5))
+                ax.add_artist(plt.Circle(WH2 * (1 + self._to_image(kart.location, proj, view)), 2, ec='b', fill=False, lw=1.5))
+                ax.add_artist(plt.Circle(WH2 * (1 + self._to_image(aim_point_world, proj, view)), 2, ec='r', fill=False, lw=1.5))
                 if planner:
                     ap = self._point_on_track(kart.distance_down_track + TRACK_OFFSET, track)
-                    ax.add_artist(plt.Circle(WH2*(1+aim_point_image), 2, ec='g', fill=False, lw=1.5))
+                    ax.add_artist(plt.Circle(WH2 * (1 + aim_point_image), 2, ec='g', fill=False, lw=1.5))
                 plt.pause(1e-3)
 
-            self.k.step(action)
+            self.k.step(pystk_action)
+
+            # UPDATE HERE: Policy Gradient / DPG Update
+            progress = (kart.overall_distance / track.length) * (t / max_frames)
+            y_target = progress + gamma * self.model(torch.tensor([aim_point_image[0], aim_point_image[1], current_vel, 25]).unsqueeze(0))
+
+            # Calculate gradient
+            gradient = y_target - action
+
+            # Perform gradient update on model weights (specifically the first layer)
+            self.optimizer.zero_grad()  # Clear previous gradients
+            loss = (0.5 * (gradient ** 2)).mean()  # Mean squared error loss
+            loss.backward()  # Backpropagate the error
+            self.optimizer.step()  # Update the model parameters
+
+            prev_aim_point_image = aim_point_image
+            previous_vel = current_vel
+
             t += 1
         return t, kart.overall_distance / track.length
 
